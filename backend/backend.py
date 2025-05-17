c8c87a0011f8a64dc2c3868d927d96ae90d4106e
aondrej ryan <aondrejryan11@gmail.com>
2025-05-17 15:32:42 -0700

    im reaqlly bad at coding

--- backend/backend.py
+++ backend/backend.py
@@ -4,6 +4,7 @@ from os import environ
 from openai import OpenAI # type: ignore
 from dotenv import load_dotenv # type: ignore
 from flask_cors import CORS # type: ignore
+import re
 
 app = Flask(__name__)
 CORS(app)
@@ -25,16 +26,18 @@ def home():
 
 
 
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     tasks = request.get_json()
-    print(tasks)
-    prompt = f"Given the following list of tasks in a JSON format, rank them in terms of priority and output it in JSON format: {tasks}"
+    
+    prompt = f"Given the following list of tasks in a JSON format,  table format: {tasks} assume work is normal working hours please give me a time schedule please create a tenaitve scehdile that will allow me to optimize energy to get all task done. please please please dont giver mer any other words please jsut give me tthe schdule formated like the following | 6:30 AM - 7:00 AM | Wake up & Morning routine | seperate each event by a space and comma"
+    print(prompt)
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         max_tokens=150,
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are a helpful assistant managing youre supervisors schedule."},
             {
                 "role": "user",
                 "content": prompt,
@@ -43,12 +46,15 @@ def analyze():
     )
     # prompt prioritized_list into gpt and ask it to create a schedule from it
     prioritized_list = response.choices[0].message.content
-    data = []
+    print(prioritized_list)
     prompt = {
-        "data": data
+        "data":"lol"
     }
-    return jsonify(lol)
+    
+    return jsonify(prompt)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+  
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,34 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const runThisCode = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/analyze', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "task1": "fulltime job",
+          "task2": "gym",
+          "task3": "meal prep",
+          "task4": "clean bathroom",
+          "task5": "walk dog"
+        })
+      });
+      const data = await response.json();
+      console.log('Data:', data);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  }
 
   return (
     <>
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <div className="text-3xl font-bold">
+        <h1>Hello world!n</h1>
+        <button onClick={runThisCode}>run this code</button>
+      </div>
     </>
   )
 }
